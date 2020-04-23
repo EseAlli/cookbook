511c675e41058a152607e1ee225f56c1e40dae6f
EseAlli <essealli.ea@gmail.com>
2020-04-23 19:11:03 +0100

    changes

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -15,40 +14,3 @@ const Routers = () => (
 );
 
 export default Routers;
-=======
-<<<<<<< HEAD
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import App from "../App";
-import Recipe from "./Recipe";
-
-const Routers = () => (
-  <Router>
-    <Switch>
-      <Route path="/" component={App} exact />
-      <Route path="/recipe/:id" component={Recipe} />
-    </Switch>
-  </Router>
-);
-
-export default Routers;
-=======
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import App from "../App";
-import Recipe from "./Recipe";
-
-const Routers = () => (
-  <Router>
-    <Switch>
-      <Route path="/" component={App} exact />
-      <Route path="/recipe/:id" component={Recipe} />
-    </Switch>
-  </Router>
-);
-
-export default Routers;
->>>>>>> 0e12c0db8534bc361781b77da184e51ebdcb0fbd
->>>>>>> 8cd950aa0191971d394012f74d0e6db777c54cc9
